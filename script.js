0df2f30c0ca14304b861aa7637a5851b5cb90ca8
AliciaD24 <101144954+AliciaD24@users.noreply.github.com>
2025-05-21 12:45:02 +0900

    Add files via upload
    Added logout + validation

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function pullApi (){
   apiToken = document.getElementById('apiToken').value;
   
   if (!apiToken) {
-    alert('Please enter your API Token before continuing.');
+    document.getElementById('user-info').innerHTML = `
+        <p>Please enter your API Token before continuing.</p>
+        `;
     return;
   } else {
   
@@ -24,7 +26,13 @@ function pullApi (){
     fetch(apiEndpoint)
       .then(response => response.json())
       .then(data => {
-        getUserInfo(data.data)
+        getUserInfo(data.data);
+      })
+      .catch(error => {
+      console.error('Error fetching API token data:', error);
+      document.getElementById('user-info').innerHTML = `
+        <p>The token you entered is invalid, please try again.</p>
+        `;
       });
   }
 }
@@ -32,17 +40,46 @@ function pullApi (){
 function getUserInfo(data) {
   
   const userInfoDiv = document.getElementById('user-info');
-  
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
+
   userInfoDiv.innerHTML = '';
-  
+
   const username = data.username;
   const level = data.level;
-  const sub = data.subscription.active;
+  var subed = data.subscription.active;
+  
+  if (subed) {
+    subed = 'Of course'
+  } else {
+    subed = 'Not yet'
+  }
   
   userInfoDiv.innerHTML = `
     <p>Username: ${username}</p>
     <p>Level: ${level}</p>
-    <p>Committed to the Crabigator? ${sub}</p>
+    <p>Committed to the Crabigator? ${subed}</p>
   `;
+  
+  login.style.display = 'none';
+  logout.style.display = 'block';
+  
+}
+
+function logout() {
+  
+  apiToken = '';
+  
+  const userInfoDiv = document.getElementById('user-info');
+  const logout = document.getElementById('logout');
+  
+  userInfoDiv.innerHTML = '';
+  
+  userInfoDiv.innerHTML = `
+    <p>Enter your WaniKani API Token below to log in:</p>
+  `;
+  
+  login.style.display = 'block';
+  logout.style.display = 'none';
   
 }
